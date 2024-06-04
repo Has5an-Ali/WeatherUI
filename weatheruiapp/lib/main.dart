3b15a4e7c03a71cc6e6ab6bb4ad5cddeaf22a55d
Hassan Ali <hassanali22857@gmail.com>
2024-06-04 11:37:02 +0500

    Next 7 days added

--- weatheruiapp/lib/main.dart
+++ weatheruiapp/lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:intl/intl.dart';
+import 'package:weatheruiapp/consts/colors.dart';
 import 'package:weatheruiapp/consts/fontstyle.dart';
 import 'package:weatheruiapp/consts/images.dart';
 import 'package:weatheruiapp/consts/strings.dart';
@@ -51,71 +52,129 @@ class WeatherUi extends StatelessWidget {
       ),
       body: Container(
         padding: EdgeInsets.all(18),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            "Islamabad".text.fontFamily(bold).letterSpacing(3).size(32).make(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(
-                  'assets/weather/01d.png',
-                  width: 85,
-                  height: 85,
-                ),
-                RichText(
-                    text: TextSpan(children: [
-                  TextSpan(
-                      text: "39$degree",
-                      style: TextStyle(
-                          color: Vx.gray800,
-                          fontSize: 28,
-                          fontFamily: semibold)),
-                  TextSpan(
-                      text: "Sunny",
-                      style: TextStyle(
-                          color: Vx.gray800,
-                          fontSize: 28,
-                          fontFamily: semibold))
-                ]))
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton.icon(
-                    onPressed: null,
-                    icon: Icon(Icons.expand_less_rounded),
-                    label: "41$degree".text.make()),
-                TextButton.icon(
-                    onPressed: null,
-                    icon: Icon(Icons.expand_more_rounded),
-                    label: "22$degree".text.make())
-              ],
-            ),
-            10.heightBox,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(3, (index) {
-                var iconlist = [clouds, humidity, windspeed];
-                var values = ["80%", "50%", "2.3 Km/h"];
-                return Column(
-                  children: [
-                    Image.asset(
-                      iconlist[index],
-                      width: 50,
-                      height: 60,
-                    ).box.gray200.roundedSM.make(),
-                    10.heightBox,
-                    values[index].text.gray400.make(),
-                  ],
-                );
-              }),
-            ),
-            10.heightBox,
-            Divider(),
-            10.heightBox
-          ],
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              "Islamabad"
+                  .text
+                  .fontFamily(bold)
+                  .letterSpacing(3)
+                  .size(32)
+                  .make(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    'assets/weather/01d.png',
+                    width: 85,
+                    height: 85,
+                  ),
+                  RichText(
+                      text: TextSpan(children: [
+                    TextSpan(
+                        text: "39$degree",
+                        style: TextStyle(
+                            color: Vx.gray800,
+                            fontSize: 28,
+                            fontFamily: semibold)),
+                    TextSpan(
+                        text: "Sunny",
+                        style: TextStyle(
+                            color: Vx.gray800,
+                            fontSize: 28,
+                            fontFamily: semibold))
+                  ]))
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton.icon(
+                      onPressed: null,
+                      icon: Icon(Icons.expand_less_rounded),
+                      label: "41$degree".text.make()),
+                  TextButton.icon(
+                      onPressed: null,
+                      icon: Icon(Icons.expand_more_rounded),
+                      label: "22$degree".text.make())
+                ],
+              ),
+              10.heightBox,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: List.generate(3, (index) {
+                  var iconlist = [clouds, humidity, windspeed];
+                  var values = ["80%", "50%", "2.3 Km/h"];
+                  return Column(
+                    children: [
+                      Image.asset(
+                        iconlist[index],
+                        width: 60,
+                        height: 60,
+                      ).box.gray200.roundedSM.make(),
+                      10.heightBox,
+                      values[index].text.gray400.make(),
+                    ],
+                  );
+                }),
+              ),
+              10.heightBox,
+              Divider(),
+              10.heightBox,
+              SizedBox(
+                height: 150,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: 6,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        padding: EdgeInsets.all(8),
+                        margin: EdgeInsets.only(right: 8),
+                        decoration: BoxDecoration(
+                            color: cardColor,
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Column(
+                          children: [
+                            "${index + 1}".text.gray200.make(),
+                            Image.asset(
+                              'assets/weather/10n.png',
+                              width: 80,
+                            ),
+                            "30$degree".text.white.make(),
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+              10.heightBox,
+              Divider(),
+              10.heightBox,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  "Next 7 Days".text.size(16).fontFamily(semibold).make(),
+                  TextButton(onPressed: () {}, child: Text("View all "))
+                ],
+              ),
+              ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 7,
+                  itemBuilder: (BuildContext context, int index) {
+                    var dayformat = DateFormat("EEEE")
+                        .format(DateTime.now().add(Duration(days: index + 1)));
+                    return Card(
+                      child: Container(
+                        child: Row(
+                          children: ["dayformat".text.make()],
+                        ),
+                      ),
+                    );
+                  })
+            ],
+          ),
         ),
       ),
     );
