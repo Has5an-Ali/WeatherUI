3386e8a76c5f3ebf089fc2fb9083c3f764039de1
Hassan Ali <hassanali22857@gmail.com>
2024-06-04 10:47:09 +0500

    Image Added

--- weatheruiapp/lib/main.dart
+++ weatheruiapp/lib/main.dart
@@ -1,8 +1,11 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:intl/intl.dart';
+import 'package:weatheruiapp/consts/fontstyle.dart';
+import 'package:weatheruiapp/consts/images.dart';
+import 'package:weatheruiapp/consts/strings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -47,7 +50,73 @@ class WeatherUi extends StatelessWidget {
         ],
       ),
       body: Container(
-        child: Column(),
+        padding: EdgeInsets.all(18),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            "Islamabad".text.fontFamily(bold).letterSpacing(3).size(32).make(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset(
+                  'assets/weather/01d.png',
+                  width: 85,
+                  height: 85,
+                ),
+                RichText(
+                    text: TextSpan(children: [
+                  TextSpan(
+                      text: "39$degree",
+                      style: TextStyle(
+                          color: Vx.gray800,
+                          fontSize: 28,
+                          fontFamily: semibold)),
+                  TextSpan(
+                      text: "Sunny",
+                      style: TextStyle(
+                          color: Vx.gray800,
+                          fontSize: 28,
+                          fontFamily: semibold))
+                ]))
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton.icon(
+                    onPressed: null,
+                    icon: Icon(Icons.expand_less_rounded),
+                    label: "41$degree".text.make()),
+                TextButton.icon(
+                    onPressed: null,
+                    icon: Icon(Icons.expand_more_rounded),
+                    label: "22$degree".text.make())
+              ],
+            ),
+            10.heightBox,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: List.generate(3, (index) {
+                var iconlist = [clouds, humidity, windspeed];
+                var values = ["80%", "50%", "2.3 Km/h"];
+                return Column(
+                  children: [
+                    Image.asset(
+                      iconlist[index],
+                      width: 60,
+                      height: 60,
+                    ).box.gray200.roundedSM.make(),
+                    10.heightBox,
+                    values[index].text.gray400.make(),
+                  ],
+                );
+              }),
+            ),
+            10.heightBox,
+            Divider(),
+            10.heightBox
+          ],
+        ),
       ),
     );
   }
